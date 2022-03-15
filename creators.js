b40b8f126a9a486cf8fe6e578aecec635a6e7468
William Thackeray <will_thack@comcast.net>
2022-03-15 16:12:44 -0600

    added sections for content stuffs and some notes on the js file for what to do with the menu.

---
+++ creators.js
@@ -0,0 +1,2 @@
+// add event listeners for anchor tags in the nav. When clicked, they should show the content for that page.
+// add if statement that if the content is already showing when clicked, do nothing.
