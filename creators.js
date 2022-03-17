cb2dcdb39b4beaf6858b955d29b8895c794afd9f
William Thackeray <will_thack@comcast.net>
2022-03-17 13:26:34 -0600

    Fixed responsive functionality for the nav

--- creators.js
+++ creators.js
@@ -3,43 +3,45 @@
 
 document.addEventListener('DOMContentLoaded', (Event) => {
 
+  // Get array of all nav p's. When one is clicked, set all to hide except the clicked one.
+  let pages = document.getElementsByClassName('menuItem');
+  let contents = document.getElementsByTagName('section');
 
-document.getElementById('home').addEventListener('click', function(){ 
-  if (document.getElementById('homeContent').className != 'shown') {
-    document.getElementsByClassName('shown').className = 'hide';
-    document.getElementById('homeContent').className = 'shown';
-  }
-});
-
-document.getElementById('streaming').addEventListener('click', function(){ 
-  if (document.getElementById('streamingContent').className != 'shown') {
-    let elem = document.getElementsByClassName('shown');
-    document.getElementsByClassName('shown').className = 'hide';
-    document.getElementById('streamingContent').className = 'shown';
-  }
-});
-
-document.getElementById('videos').addEventListener('click', function(){ 
-  if (document.getElementById('videosContent').className != 'shown') {
-    document.getElementsByClassName('shown').className = 'hide';
-    document.getElementById('videosContent').className = 'shown';
-  }
-});
-
-document.getElementById('community').addEventListener('click', function(){ 
-  if (document.getElementById('communityContent').className != 'shown') {
-    document.getElementsByClassName('shown').className = 'hide';
-    document.getElementById('communityContent').className = 'shown';
-  }
-});
-
-document.getElementById('branding').addEventListener('click', function(){ 
-  if (document.getElementById('brandingContent').className != 'shown') {
-    document.getElementsByClassName('shown').className = 'hide';
-    document.getElementById('brandingContent').className = 'shown';
-  }
-});
-
+  // to show the home content
+  pages[0].addEventListener('click', function(){
+    for (let i = 0; i < pages.length; i++) {
+      contents[i].className = 'hide';
+    }
+    contents[0].className = '';
+  });
+  // to show the Live Streaming content
+  pages[1].addEventListener('click', function(){
+    for (let i = 0; i < pages.length; i++) {
+      contents[i].className = 'hide';
+    }
+    contents[1].className = '';
+  });
+  // to show the Videos content
+  pages[2].addEventListener('click', function(){
+    for (let i = 0; i < pages.length; i++) {
+      contents[i].className = 'hide';
+    }
+    contents[2].className = '';
+  });
+  // to show the community content
+  pages[3].addEventListener('click', function(){
+    for (let i = 0; i < pages.length; i++) {
+      contents[i].className = 'hide';
+    }
+    contents[3].className = '';
+  });
+  // to show the branding content
+  pages[4].addEventListener('click', function(){
+    for (let i = 0; i < pages.length; i++) {
+      contents[i].className = 'hide';
+    }
+    contents[4].className = '';
+  });
 
 });
 //.addEventListener('click', function(){ alert('it worked!')});
