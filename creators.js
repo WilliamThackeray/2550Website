82b7512f6b3b1087111177297873e58fc1147516
William Thackeray <will_thack@comcast.net>
2022-04-07 20:26:46 -0600

    Finished Refactor.

--- creators.js
+++ creators.js
@@ -42,5 +42,21 @@ pages[4].addEventListener('click', function(){
   contents[4].className = '';
 });
 
+// Toggle Theme Button functionality here
+let themeSheet = '<link id="theme" rel="stylesheet" href="theme.css">'
 
+let bool = false;
 
+$("#toggle").click(function(){
+  console.log("Clicked");
+  // check if theme.css is in head
+  if (bool) {
+    console.log("sheet is in html");
+    $("#theme").remove();
+    bool = false;
+  } else {
+    console.log("sheet is not in html");
+    $("#baseStyling").after(themeSheet);
+    bool = true;
+  }
+});
